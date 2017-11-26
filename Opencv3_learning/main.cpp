64246c2081cc9c698112cafc3801cdc8b170e8a5
cb <cb@cb.local>
2017-11-26 10:30:43 +0800

    Initial Commit

---
+++ Opencv3_learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Opencv3_learning
+//
+//  Created by cb on 2017/11/26.
+//  Copyright © 2017年 cb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
